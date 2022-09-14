9eb2c80f8921e78be5127b038528d78ee10e20a4
L Carver <wifihum@gmail.com>
2022-09-14 13:41:12 -0700

    apply VS document formatting

--- source/app.cpp
+++ source/app.cpp
@@ -15,31 +15,31 @@ using std::cout;
 // entry point
 //----------------------------------------------------------------------
 int main() {
-    cout << "\nWelcome to Coin Toss!\n\n";
-    cout << "How many coin tosses? ";
-    int tosses;
-    cin >> tosses;
+	cout << "\nWelcome to Coin Toss!\n\n";
+	cout << "How many coin tosses? ";
+	int tosses;
+	cin >> tosses;
 
-    // display initial coin state
-    Coin suzyBuck;
-    cout << "\nInitial side up: " << suzyBuck.getSideUp() << '\n';
+	// display initial coin state
+	Coin suzyBuck;
+	cout << "\nInitial side up: " << suzyBuck.getSideUp() << '\n';
 
-    // simulate coin tosses and display results of each
-    for (int i = 0; i < tosses; i++) {
-        suzyBuck.toss();
-        cout << "\nToss " << i+1 << ": " << suzyBuck.getSideUp();
-    }
-    cout << '\n';
+	// simulate coin tosses and display results of each
+	for (int i = 0; i < tosses; i++) {
+		suzyBuck.toss();
+		cout << "\nToss " << i + 1 << ": " << suzyBuck.getSideUp();
+	}
+	cout << '\n';
 
-    // display numbers of heads and tails tossed
-    cout << "\nNumber of heads tossed: " << suzyBuck.getHeadsCount();
-    cout << "\nNumber of tails tossed: " << suzyBuck.getTailsCount();
+	// display numbers of heads and tails tossed
+	cout << "\nNumber of heads tossed: " << suzyBuck.getHeadsCount();
+	cout << "\nNumber of tails tossed: " << suzyBuck.getTailsCount();
 
-    // use the history vector
-    cout << "\n\nFirst toss was: " << suzyBuck.getTossResult(1);
+	// use the history vector
+	cout << "\n\nFirst toss was: " << suzyBuck.getTossResult(1);
 
-    cout << "\n\nGoodbye!\n";
+	cout << "\n\nGoodbye!\n";
 
-    return 0;
+	return 0;
 }
 
